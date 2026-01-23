4f4cb254423603a57226363ffa6160b23c9b83a3
otavioalbe <otavioalbe@gmail.com>
2026-01-23 03:31:49 -0300

    feat: docker compose file, postgres database setup, auth-service setup

--- auth-service/src/main/java/com/application/authService/AuthServiceApplication.java
+++ auth-service/src/main/java/com/application/authService/AuthServiceApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.application.authService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
