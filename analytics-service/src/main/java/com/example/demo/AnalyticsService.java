3f5dc4656fcdc191516205b72af6fd90b063292e
Otávio De Carli Albuquerque <otavioalbe@gmail.com>
2026-03-17 10:08:32 -0300

    feat: analytics service setup

--- analytics-service/src/main/java/com/example/demo/AnalyticsService.java
+++ analytics-service/src/main/java/com/example/demo/AnalyticsService.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthServiceApplication {
+public class AnalyticsService {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
+		SpringApplication.run(AnalyticsService.class, args);
 	}
 
 }
