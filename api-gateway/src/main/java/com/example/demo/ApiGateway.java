08ad09a329915497a0ae2c550f47b651674dd733
Otávio De Carli Albuquerque <otavioalbe@gmail.com>
2026-03-17 09:56:03 -0300

    refactor: api-gateway application rename

--- api-gateway/src/main/java/com/example/demo/ApiGateway.java
+++ api-gateway/src/main/java/com/example/demo/ApiGateway.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AuthServiceApplication {
+public class ApiGateway {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
+		SpringApplication.run(ApiGateway.class, args);
 	}
 
 }
